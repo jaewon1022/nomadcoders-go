c96287cc368d64923013c03c5be961d3241a810e
andyjaewon <andyjaewon@naver.com>
2024-07-16 11:51:12 +0900

    #3.1

--- main.go
+++ main.go
@@ -9,22 +9,37 @@ import (
 var errHttpRequestFailed = errors.New("http request failed")
 
 func main() {
+	var results = make(map[string]string)
+
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
 		"https://www.amazon.com/",
 		"https://www.facebook.com/",
 		"https://www.instagram.com/",
+		"http://asdf",
 	}
 
 	for _, url := range urls {
-		hitURL(url)
+		err := hitURL(url)
+
+		result := "OK"
+
+		if err != nil {
+			result = "FAILED"
+		}
+
+		results[url] = result
+	}
+
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 
 func hitURL(url string) error {
-	fmt.Println("Checking URL...")
-	
+	fmt.Println("Checking URL :", url)
+
 	resp, err := http.Get(url)
 
 	if err != nil || resp.StatusCode >= 400 {
