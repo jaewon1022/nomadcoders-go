c58b9f58aa12cb50224e764c1ef7cc524f3b599c
andyjaewon <andyjaewon@naver.com>
2024-07-16 13:23:33 +0900

    3.3

--- main.go
+++ main.go
@@ -6,14 +6,24 @@ import (
 )
 
 func main() {
-	go count("aaa")
-	go count("bbb")
-	time.Sleep(time.Second * 5)
+	channel := make(chan bool)
+
+	people := [2]string {"nick", "test"}
+
+	for _,person := range people {
+		go isCool(person, channel)
+		
+	}
+
+	fmt.Println(<- channel)
+	fmt.Println(<- channel)
 }
 
-func count(person string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(person, i)
-		time.Sleep(time.Second * 1)
-	}
+func isCool(person string, channel chan bool)  {
+	fmt.Println("isCool request received")
+	time.Sleep(time.Second * 2)
+
+	fmt.Println(person, "is Cool")
+
+	channel <- true
 }
